dc404d3e7ff1307fe178aa1fcc2b626032d9c58f
crj0322 <crj_0322@qq.com>
2018-12-13 10:02:56 +0800

    fix loss function

--- yolov3/utils.py
+++ yolov3/utils.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import colorsys
 import random
 import numpy as np
+import xml.etree.ElementTree as ET
+from tqdm import tqdm
+import os
 
 
 def box_iou(box1, box2):
@@ -31,7 +34,7 @@ def letterbox_image(image, size):
     return boxed_image
 
 def draw_bbox(image, class_names, out_boxes, out_scores, out_classes):
-    font = ImageFont.truetype(font='font/FiraMono-Medium.otf', size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
+    # font = ImageFont.truetype(font='font/FiraMono-Medium.otf', size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
     thickness = (image.size[0] + image.size[1]) // 300
     
     # Generate colors for drawing bounding boxes.
@@ -50,7 +53,7 @@ def draw_bbox(image, class_names, out_boxes, out_scores, out_classes):
 
         label = '{} {:.2f}'.format(predicted_class, score)
         draw = ImageDraw.Draw(image)
-        label_size = draw.textsize(label, font)
+        label_size = draw.textsize(label)
 
         left,top, right, bottom = box
         top = max(0, np.floor(top + 0.5).astype('int32'))
@@ -72,9 +75,162 @@ def draw_bbox(image, class_names, out_boxes, out_scores, out_classes):
         draw.rectangle(
             [tuple(text_origin), tuple(text_origin + label_size)],
             fill=colors[c])
-        draw.text(text_origin, label, fill=(0, 0, 0), font=font)
+        draw.text(text_origin, label, fill=(0, 0, 0))
         del draw
         
     plt.figure(figsize=(10,8))
     plt.imshow(image)
     
+def read_image(file_list, image_path):
+    """
+    Arguments:
+        file_list: xml file names.
+        image_path: image file path.
+    """
+    X = []
+    for i, file in enumerate(tqdm(file_list)):
+        image = Image.open(image_path + file[:-4] + '.jpg')
+        # image = letterbox_image(image, (416, 416))
+        image = image.resize((416, 416),Image.ANTIALIAS)
+        image = np.array(image, dtype='uint8')
+        image = np.expand_dims(image, 0)
+        X.append(image)
+    
+    X = np.concatenate(X, axis=0).astype(np.float32)
+    return X/255.
+
+def read_boxes(file_list, xml_path, class_dict, anchors):
+    """
+    Arguments:
+        file_list: xml file names.
+        xml_path: xml file path.
+    Return:
+        list of true boxes (xymin xymax format) in each image.
+    """
+    y1 = []
+    y2 = []
+    y3 = []
+    for file in tqdm(file_list):
+        tree =  ET.ElementTree(file=xml_path + os.sep + file)
+        
+        # image size
+        w = []
+        h = []
+        for elem in tree.iterfind('size/width'):
+            w.append(int(elem.text))
+        for elem in tree.iterfind('size/height'):
+            h.append(int(elem.text))
+
+        assert len(w) == 1 and len(h) == 1
+        image_wh = np.array([*w, *h])
+        
+        xmin = []
+        xmax = []
+        ymin = []
+        ymax = []
+        box_class = []
+        for elem in tree.iterfind('object/bndbox/xmin'):
+            xmin.append(int(elem.text))
+
+        for elem in tree.iterfind('object/bndbox/xmax'):
+            xmax.append(int(elem.text))
+
+        for elem in tree.iterfind('object/bndbox/ymin'):
+            ymin.append(int(elem.text))
+
+        for elem in tree.iterfind('object/bndbox/ymax'):
+            ymax.append(int(elem.text))
+
+        for elem in tree.iterfind('object/name'):
+            assert elem.text in class_dict.keys()
+            box_class.append(class_dict[elem.text])
+
+        xmin = np.array(xmin).reshape((-1, 1))
+        xmax = np.array(xmax).reshape((-1, 1))
+        ymin = np.array(ymin).reshape((-1, 1))
+        ymax = np.array(ymax).reshape((-1, 1))
+        box_class = np.array(box_class).reshape((-1, 1))
+
+        box = np.concatenate([xmin, ymin, xmax, ymax, box_class], axis=-1)
+        
+        # convert to y format
+        y_true = boxes_to_y(box, anchors, len(class_dict), image_wh)
+        y1.append(np.expand_dims(y_true[0], 0))
+        y2.append(np.expand_dims(y_true[1], 0))
+        y3.append(np.expand_dims(y_true[2], 0))
+
+    y1 = np.concatenate(y1, axis=0)
+    y2 = np.concatenate(y2, axis=0)
+    y3 = np.concatenate(y3, axis=0)
+        
+    return [y1, y2, y3]
+
+def boxes_to_y(true_boxes, anchors, num_classes, image_wh):
+    """
+    transfer true boxes to yolo y format.
+    Arguments:
+        true_boxes: bbox absolute value in image_wh of one image,
+                    value as (xmin, ymin, xmax, ymax, class), shape(?, 5).
+        anchors: anchor boxe size array, shape(num_anchors, 2).
+        num_classes: total class num.
+        image_wh: true input image size of (w, h).
+        
+    Returns:
+        y_true: list of yolo feature map fomat,
+                shape(grid w, grid h, num_anchors, 5+num_classes),
+                box xywh info normalize to (0, 1).
+    """
+    num_anchors = anchors.shape[0] // 3
+    box_class = true_boxes[:, 4].astype(np.int32)
+    xymin, xymax = true_boxes[:, 0:2], true_boxes[:, 2:4]
+    
+    input_size = np.array([416, 416])
+    
+    # calculate box center xy and wh, range(0, 416).
+    boxes_wh = xymax - xymin
+    boxes_xy = xymin + boxes_wh//2
+    
+    # normalize to range(0, 1)
+    boxes_xy = boxes_xy/image_wh
+    
+    # grid shape
+    grid_wh = [input_size//32, input_size//16, input_size//8]  # [[13, 13], [26, 26], [52, 52]]
+    grid_boxes_xy = [boxes_xy * grid_wh[i] for i in range(3)]  # to grid scale, range(0, grid_wh).
+    grid_index = [np.floor(grid_boxes_xy[i]) for i in range(3)]
+    # boxes_xy = [(boxes_xy[i] - grid_index[i]) for i in range(3)]  # size respect to one grid, range(0, 1).
+    
+    # true size of xy min max cordinates relative to grid left top corner.
+    anchor_xymax = anchors/2
+    anchor_xymin = -anchor_xymax
+    box_xymax = boxes_wh/2
+    box_xymin = -box_xymax
+    
+    # create y_true.
+    y_true = [np.zeros((grid_wh[i][1], grid_wh[i][0], num_anchors, 5+num_classes), dtype='float32') for i in range(3)]
+    
+    # iterate on each box
+    num_boxes = true_boxes.shape[0]
+    for box_index in range(num_boxes):
+        # calculate iou.
+        box1 = np.concatenate([box_xymin[box_index], box_xymax[box_index]]).reshape(1, -1)
+        box2 = np.concatenate([anchor_xymin, anchor_xymax], axis=-1)
+        iou = box_iou(box1, box2)
+        
+        # select the best anchor
+        anchor_index = np.argmax(iou)
+        layer_index = 2 - anchor_index//3
+        layer_anchor_index = anchor_index % 3
+        
+        box_xy = boxes_xy[box_index]  # shape(2,)
+        # box_wh = boxes_wh[box_index]/anchors[anchor_index]  # shape(2,)
+        box_wh = boxes_wh[box_index]/image_wh  # shape(2,)， range(0, 1)
+        
+        #  fill in y_true.
+        w = grid_index[layer_index][box_index, 0].astype('int32')
+        h = grid_index[layer_index][box_index, 1].astype('int32')
+        y_true[layer_index][h, w, layer_anchor_index, :2] = box_xy
+        y_true[layer_index][h, w, layer_anchor_index, 2:4] = box_wh
+        y_true[layer_index][h, w, layer_anchor_index, 4:5] = 1
+        y_true[layer_index][h, w, layer_anchor_index, 5+box_class[box_index]] = 1
+        
+    return y_true
---
+++ yolov3/yolo_model.py
@@ -0,0 +1,263 @@
+from keras.layers import Input, Conv2D, Add, ZeroPadding2D, UpSampling2D, Concatenate, Lambda
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras.regularizers import l2
+from keras import backend as K
+import tensorflow as tf
+import numpy as np
+
+
+def conv_unit(x, filters, kernels, padding='same', strides=1):
+    x = Conv2D(filters, kernels,
+               padding=padding,
+               strides=strides,
+               use_bias=False,
+               kernel_regularizer=l2(5e-4))(x)
+    x = BatchNormalization()(x)
+    x = LeakyReLU(alpha=0.1)(x)
+    
+    return x
+
+def residual_block(inputs, filters, n):
+    x = ZeroPadding2D(((1, 0),(1, 0)))(inputs)
+    x = conv_unit(x, filters, (3, 3), padding='valid', strides=2)
+    for i in range(n):
+        y = conv_unit(x, filters//2, (1, 1))
+        y = conv_unit(y, filters, (3, 3))
+        x = Add()([x, y])
+
+    return x
+
+def darknet53(inputs):
+    x = conv_unit(inputs, 32, (3, 3))
+    x = residual_block(x, 64, 1)
+    x = residual_block(x, 128, 2)
+    x = residual_block(x, 256, 8)
+    x = residual_block(x, 512, 8)
+    x = residual_block(x, 1024, 4)
+    
+    return x
+
+def top_conv(x, filters, out_dim):
+    x = conv_unit(x, filters//2, (1, 1))
+    x = conv_unit(x, filters, (3, 3))
+    x = conv_unit(x, filters//2, (1, 1))
+    x = conv_unit(x, filters, (3, 3))
+    x = conv_unit(x, filters//2, (1, 1))
+    
+    y = conv_unit(x, filters, (3, 3))
+    y = Conv2D(out_dim, (1, 1),
+               padding='same',
+               # kernel_initializer=init_func,
+               kernel_regularizer=l2(5e-4))(y)
+    
+    return x, y
+
+def yolo_model(inputs, anchors, num_classes):
+    num_anchors = anchors.shape[0] // 3
+    # def my_init(shape, dtype=None):
+    #     return K.random_normal(shape, stddev=1e-4, dtype=dtype)
+
+    darknet = Model(inputs=inputs, outputs=darknet53(inputs))
+    x, y1 = top_conv(darknet.output, 1024, num_anchors*(num_classes+5))
+    
+    x = conv_unit(x, 256, (1, 1))
+    x = UpSampling2D(2)(x)
+    x = Concatenate()([x, darknet.layers[152].output])
+    x, y2 = top_conv(x, 512, num_anchors*(num_classes+5))
+    
+    x = conv_unit(x, 128, (1, 1))
+    x = UpSampling2D(2)(x)
+    x = Concatenate()([x, darknet.layers[92].output])
+    x, y3 = top_conv(x, 256, num_anchors*(num_classes+5))
+
+    # boxes = Lambda(feat_to_boxes, name='feat_to_boxes',
+    #     arguments={'anchors': anchors, 'num_classes': num_classes})([y1, y2, y3])
+    
+    return [y1, y2, y3]
+
+def feat_to_boxes(feats, anchors, num_classes):
+    anchor_mask = [[6,7,8], [3,4,5], [0,1,2]]
+    num_anchors = len(anchor_mask)
+    input_wh = K.constant([416, 416])
+    batch_size = K.shape(feats[0])[0]
+
+    out_boxes = []
+
+    # iterate along 3 layers.
+    for i in range(3):
+        # get anchor tensor
+        anchors_tensor = K.reshape(K.constant(anchors[anchor_mask[i]]), [1, 1, 1, num_anchors, 2])
+        
+        # get grid
+        grid_hw = K.shape(feats[i])[1:3]
+        grid_y = K.tile(K.reshape(K.arange(0, stop=grid_hw[0]), [-1, 1, 1, 1]), [1, grid_hw[1], 1, 1])
+        grid_x = K.tile(K.reshape(K.arange(0, stop=grid_hw[1]), [1, -1, 1, 1]), [grid_hw[0], 1, 1, 1])
+        grid = K.concatenate([grid_x, grid_y])
+        grid = K.cast(grid, K.dtype(feats[i]))
+        
+        # get prediction values in range(0, 1)
+        feature = K.reshape(feats[i], [-1, grid_hw[0], grid_hw[1], num_anchors, num_classes + 5])
+        box_xy = K.sigmoid(feature[..., :2])
+        box_wh = K.exp(feature[..., 2:4])
+        box_confidence = K.sigmoid(feature[..., 4:5])
+        box_class_probs = K.sigmoid(feature[..., 5:])
+        box_xy = (box_xy + grid) / K.cast(grid_hw[::-1], K.dtype(feature))
+        box_wh = box_wh * anchors_tensor / K.cast(input_wh, K.dtype(feature))
+        
+        boxes = K.concatenate((box_xy, box_wh, box_confidence, box_class_probs))
+        boxes = K.reshape(boxes, [batch_size, -1, num_classes + 5])
+        out_boxes.append(boxes)
+
+    return K.concatenate(out_boxes, axis=0)
+
+def yolo_loss(inputs, anchors, num_classes, ignore_threshold=0.5):
+    y_pred = inputs[:3]
+    y_true = inputs[3:]
+    anchor_mask = [[6,7,8], [3,4,5], [0,1,2]]
+    num_anchors = len(anchor_mask)
+    input_wh = K.constant([416, 416], K.dtype(y_pred[0]))
+    batch_size = K.shape(y_pred[0])[0]
+    # m = K.cast(batch_size, K.dtype(y_pred[0]))
+    loss = 0
+    xy_loss = 0
+    wh_loss = 0
+    iou_loss = 0
+    bg_loss = 0
+    class_loss = 0
+    
+    # iterate along 3 layers.
+    for i in range(3):
+        # get anchor tensor
+        anchors_tensor = K.reshape(K.constant(anchors[anchor_mask[i]]), [1, 1, 1, num_anchors, 2])
+        
+        # get grid
+        grid_hw = K.shape(y_pred[i])[1:3]
+        grid_y = K.tile(K.reshape(K.arange(0, stop=grid_hw[0]), [-1, 1, 1, 1]), [1, grid_hw[1], 1, 1])
+        grid_x = K.tile(K.reshape(K.arange(0, stop=grid_hw[1]), [1, -1, 1, 1]), [grid_hw[0], 1, 1, 1])
+        grid = K.concatenate([grid_x, grid_y])
+        grid = K.cast(grid, K.dtype(y_pred[i]))
+        
+        # get prediction values
+        feature = K.reshape(y_pred[i], [-1, grid_hw[0], grid_hw[1], num_anchors, num_classes + 5])
+        raw_box_xy = feature[..., :2]
+        raw_box_wh = feature[..., 2:4]
+        raw_box_confidence = feature[..., 4:5]
+        raw_box_class_probs = feature[..., 5:]
+        box_xy = K.sigmoid(raw_box_xy)
+        box_wh = K.exp(raw_box_wh)
+        # box_confidence = K.sigmoid(feature[..., 4:5])
+        # box_class_probs = K.sigmoid(feature[..., 5:])
+        grid_wh = K.cast(grid_hw[::-1], K.dtype(feature))
+        box_xy = (box_xy + grid) / grid_wh
+        box_wh = box_wh * anchors_tensor / input_wh
+        
+        # get true values
+        ture_xy = y_true[i][..., :2]
+        true_wh = y_true[i][..., 2:4]
+        true_confidence = y_true[i][..., 4:5]
+        true_class_probs = y_true[i][..., 5:]
+        raw_true_xy = ture_xy * grid_wh - grid
+        raw_true_wh = K.log(y_true[i][..., 2:4] / anchors[anchor_mask[i]] * input_wh)
+        raw_true_wh = K.switch(true_confidence, raw_true_wh, K.zeros_like(raw_true_wh)) # avoid log(0)=-inf
+        
+        # calculate loss
+
+        # scale cordinate loss
+        lambda_cord = 2 - y_true[i][...,2:3]*y_true[i][...,3:4]
+        
+        # shape(batch, grid_h, grid_w, num_anchors, 2)
+        tmp_xy_loss = lambda_cord * true_confidence * K.square(raw_box_xy - raw_true_xy)
+        tmp_wh_loss = 0.5 * lambda_cord * true_confidence * K.square(raw_box_wh - raw_true_wh)
+        
+        # bg loss ignore iou more than iou_threshold
+        # Find ignore mask, iterate over each of batch.
+        pred_box = K.concatenate((box_xy, box_wh))
+        ignore_mask = tf.TensorArray(K.dtype(y_true[0]), size=1, dynamic_size=True)
+        object_mask_bool = K.cast(true_confidence, 'bool')
+        def loop_body(b, ignore_mask):
+            true_box = tf.boolean_mask(y_true[i][b,...,0:4], object_mask_bool[b,...,0])
+            iou = box_iou(pred_box[b], true_box)
+            best_iou = K.max(iou, axis=-1)
+            ignore_mask = ignore_mask.write(b, K.cast(best_iou<ignore_threshold, K.dtype(true_box)))
+            return b+1, ignore_mask
+        _, ignore_mask = K.control_flow_ops.while_loop(lambda b,*args: b<batch_size, loop_body, [0, ignore_mask])
+        ignore_mask = ignore_mask.stack()
+        ignore_mask = K.expand_dims(ignore_mask, -1)
+        
+        iou_crossentropy = K.binary_crossentropy(true_confidence, raw_box_confidence, from_logits=True)
+        tmp_iou_loss = true_confidence * iou_crossentropy
+        tmp_bg_loss = ignore_mask * (1-true_confidence) * iou_crossentropy
+        
+        tmp_class_loss = true_confidence * K.binary_crossentropy(true_class_probs, raw_box_class_probs, from_logits=True)
+        
+        box_count = K.maximum(K.sum(true_confidence), 1)
+        bg_count = K.maximum(K.sum(ignore_mask*(1-true_confidence)), 1)
+        xy_loss += K.sum(tmp_xy_loss)/box_count
+        wh_loss += K.sum(tmp_wh_loss)/box_count
+        iou_loss += K.sum(tmp_iou_loss)/box_count
+        bg_loss += K.sum(tmp_bg_loss)/bg_count
+        class_loss += K.sum(tmp_class_loss)/box_count
+
+    loss = xy_loss + wh_loss + iou_loss + bg_loss + class_loss
+
+    # for print
+    # loss = K.stack([xy_loss, wh_loss, iou_loss, bg_loss, class_loss])
+        
+    return loss
+
+def box_iou(b1, b2):
+    '''Return iou tensor
+    Parameters
+    ----------
+    b1: tensor, shape=(i1,...,iN, 4), xywh
+    b2: tensor, shape=(j, 4), xywh
+    Returns
+    -------
+    iou: tensor, shape=(i1,...,iN, j)
+    '''
+
+    # Expand dim to apply broadcasting.
+    b1 = K.expand_dims(b1, -2)
+    b1_xy = b1[..., :2]
+    b1_wh = b1[..., 2:4]
+    b1_wh_half = b1_wh/2.
+    b1_mins = b1_xy - b1_wh_half
+    b1_maxes = b1_xy + b1_wh_half
+
+    # Expand dim to apply broadcasting.
+    b2 = K.expand_dims(b2, 0)
+    b2_xy = b2[..., :2]
+    b2_wh = b2[..., 2:4]
+    b2_wh_half = b2_wh/2.
+    b2_mins = b2_xy - b2_wh_half
+    b2_maxes = b2_xy + b2_wh_half
+
+    intersect_mins = K.maximum(b1_mins, b2_mins)
+    intersect_maxes = K.minimum(b1_maxes, b2_maxes)
+    intersect_wh = K.maximum(intersect_maxes - intersect_mins, 0.)
+    intersect_area = intersect_wh[..., 0] * intersect_wh[..., 1]
+    b1_area = b1_wh[..., 0] * b1_wh[..., 1]
+    b2_area = b2_wh[..., 0] * b2_wh[..., 1]
+    iou = intersect_area / (b1_area + b2_area - intersect_area)
+
+    return iou
+    
+def main():
+    # read anchors
+    with open('model/yolo_anchors.txt') as f:
+        anchors = f.readline()
+        anchors = [float(x) for x in anchors.split(',')]
+        anchors = np.array(anchors).reshape(-1, 2)
+
+    # build model
+    inputs = Input((416, 416, 3))
+    y1, y2, y3 = yolo_model(inputs, anchors, 80)
+    model = Model(inputs, [y1, y2, y3])
+    model.load_weights('model/yolo.h5')
+    base_model = Model(model.input, [model.layers[-4].output, model.layers[-5].output, model.layers[-6].output])
+    base_model.save('model/yolo_base.h5')
+
+if __name__ == '__main__':
+    main()
